45dfca8f3af53fa01e5c99796f790d911b3677e9
Thaksheel <thaksheelspillai@gmail.com>
2024-08-31 20:39:12 +0530

    created vercel config

--- ecommerse_api/settings.py
+++ ecommerse_api/settings.py
@@ -131,7 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+
+STATIC_URL = '/static/'  # URL to use when referring to static files
+STATICFILES_DIRS = [     # Additional locations the staticfiles app will traverse
+    BASE_DIR / "static", 
+]
+
+# Directory where static files will be collected
+STATIC_ROOT = BASE_DIR / "staticfiles"  
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
